aaf9967b262311e54f4aa151d339fb5eb0db5968
gam24 <23352901+gaam24@users.noreply.github.com>
2023-04-27 00:40:36 +0200

    Remove unused icons

--- TinyGUI.UI.Windows/Properties/Resources.Designer.cs
+++ TinyGUI.UI.Windows/Properties/Resources.Designer.cs
@@ -63,26 +63,6 @@ namespace TinyGUI.UI.Windows.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap Dashboard {
-            get {
-                object obj = ResourceManager.GetObject("Dashboard", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Progress {
-            get {
-                object obj = ResourceManager.GetObject("Progress", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap Settings {
             get {
                 object obj = ResourceManager.GetObject("Settings", resourceCulture);
